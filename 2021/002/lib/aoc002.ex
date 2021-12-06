b0025b9a1669eb3e258cdb9d51fede38dc0dc6d8
AxlER8R <axl@axler8r.io>
2021-12-07 11:36:47 +1300

    feat: implement aoc 02.1

---
+++ 2021/002/lib/aoc002.ex
@@ -0,0 +1,31 @@
+defmodule Aoc002 do
+  def problem1 do
+    {x, y} = File.read!("data/input")
+    |> extract_position()
+
+    x * y
+  end
+
+  defp extract_position(data) do
+    do_extract_position(data, {0, 0})
+  end
+
+  defp do_extract_position(<<>>, acc) do
+    acc
+  end
+
+  defp do_extract_position("forward " <> <<p, 10, tail::binary>> = _data, {f, d} = _acc) do
+    change = String.to_integer(<<p>>);
+    do_extract_position(tail, {f + change, d})
+  end
+
+  defp do_extract_position("down " <> <<p, 10, tail::binary>> = _data, {f, d} = _acc) do
+    change = String.to_integer(<<p>>);
+    do_extract_position(tail, {f, d + change})
+  end
+
+  defp do_extract_position("up " <> <<p, 10, tail::binary>> = _data, {f, d} = _acc) do
+    change = String.to_integer(<<p>>);
+    do_extract_position(tail, {f, d - change})
+  end
+end
