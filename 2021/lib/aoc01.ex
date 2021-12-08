76aa491d1b62926802dc82175cb3cb2c22e82b6e
AxlER8R <axl@axler8r.io>
2021-12-08 13:24:11 +1300

    feat: optimize aoc 01.1 and aoc 01.2

--- 2021/lib/aoc01.ex
+++ 2021/lib/aoc01.ex
@@ -1,42 +1,29 @@
 defmodule AdventOfCode.Day01 do
+  def input do
+    input =
+      File.stream!("data/01/input")
+      |> Stream.map(&String.to_integer(String.trim(&1)))
+
+    input
+  end
+
+  @spec problem1 :: non_neg_integer
   def problem1 do
-    {_, answer} = File.stream!("data/01/input")
-    |> Enum.map(&String.trim/1)
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.reduce({0, -1}, fn (current, {previous, acc} = _acc) ->
-      cond do
-        current > previous -> {current, acc + 1}
-        true -> {current, acc}
-      end
-    end)
+    answer =
+      input()
+      |> Stream.chunk_every(2, 1, :discard)
+      |> Enum.count(fn [a, b] -> a < b end)
 
     answer
   end
 
+  @spec problem2 :: non_neg_integer
   def problem2 do
-    {_, answer} = File.stream!("data/01/input")
-    |> Enum.map(&String.trim/1)
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.to_list()
-    |> chunk([])
-    |> Enum.reverse()
-    |> Enum.map(&Enum.sum/1)
-    |> Enum.reduce({0, -1}, fn (current, {previous, acc} = _acc) ->
-      cond do
-        current > previous -> {current, acc + 1}
-        true -> {current, acc}
-      end
-    end)
+    answer =
+      input()
+      |> Stream.chunk_every(4, 1, :discard)
+      |> Enum.count(fn [a, _, _, b] -> a < b end)
 
     answer
-  end
-
-  defp chunk([a, b, c], acc) do
-    [[a, b, c ] | acc]
-  end
-
-  defp chunk([a, b, c | rest] = _list, acc) do
-    acc = [[a, b, c] | acc]
-    chunk([b, c | rest], acc)
   end
 end
