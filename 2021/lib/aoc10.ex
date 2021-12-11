a417a46a3f91989f2391625d053213ba61eecca5
AxlER8R <axl@axler8r.io>
2021-12-12 08:31:32 +1300

    feat: implement aoc 10.1 and aoc 10.2

--- 2021/lib/aoc10.ex
+++ 2021/lib/aoc10.ex
@@ -1,59 +1,55 @@
+defmodule AdventOfCode.Day10.Syntax do
+  @spec parse(binary) :: :ok | {:corrupted, byte} | {:incomplete, [41 | 62 | 93 | 125]}
+  def parse(line), do: parse(line, [])
+
+  # Opening
+  defp parse(<<?(, rest::binary>>, stack), do: parse(rest, [?) | stack])
+  defp parse(<<?[, rest::binary>>, stack), do: parse(rest, [?] | stack])
+  defp parse(<<?{, rest::binary>>, stack), do: parse(rest, [?} | stack])
+  defp parse(<<?<, rest::binary>>, stack), do: parse(rest, [?> | stack])
+
+  # Closing
+  defp parse(<<char, rest::binary>>, [char | stack]), do: parse(rest, stack)
+
+  # Base/error cases
+  defp parse(<<char, _rest::binary>>, _stack), do: {:corrupted, char}
+  defp parse(<<>>, []), do: :ok
+  defp parse(<<>>, stack), do: {:incomplete, stack}
+end
+
 defmodule AdventOfCode.Day10 do
-  @block %{"(" => ")", "[" => "]", "{" => "}", "<" => ">"}
-  @error %{")" => 3, "]" => 57, "}" => 1197, ">" => 25137}
-  @autocorrect %{")" => 1, "]" => 2, "}" => 3, ">" => 4}
+  import AdventOfCode.Day10.Syntax
 
   @spec problem1 :: number
   def problem1 do
-    input()
-    |> Enum.map(&parse/1)
-    |> Enum.filter(&(elem(&1, 0) == :error))
-    |> Enum.map(fn {_, v} -> v end)
-    |> Enum.sum()
+    score = %{?) => 3, ?] => 57, ?} => 1197, ?> => 25137}
+
+    Enum.sum(
+      for line <- input(),
+          {:corrupted, char} <- [parse(line)],
+          do: score[char]
+    )
   end
 
   @spec problem2 :: number
   def problem2 do
-    results =
-      input()
-      |> Enum.map(&parse/1)
-      |> Enum.filter(&(elem(&1, 0) == :ok))
-      |> Enum.map(fn {_, v} -> v end)
-      |> Enum.sort()
+    score = %{?) => 1, ?] => 2, ?} => 3, ?> => 4}
 
-    middle = results |> length() |> div(2)
-    Enum.at(results, middle)
+    scores =
+      Enum.sort(
+        for line <- input(),
+            {:incomplete, chars} <- [parse(line)],
+            do: Enum.reduce(chars, 0, fn char, acc -> acc * 5 + score[char] end)
+      )
+
+    Enum.fetch!(scores, div(length(scores), 2))
   end
 
   defp input do
     input =
       File.read!("data/10/input")
       |> String.split("\n", trim: true)
-      |> Enum.map(&String.codepoints/1)
 
     input
-  end
-
-  defp parse(line, open \\ [])
-
-  defp parse([head | tail], open) when head in ["(", "[", "{", "<"] do
-    parse(tail, [head | open])
-  end
-
-  defp parse([head | tail], [open_head | open_tail]) do
-    if head == @block[open_head] do
-      parse(tail, open_tail)
-    else
-      {:error, @error[head]}
-    end
-  end
-
-  defp parse(_, open) do
-    score =
-      Enum.reduce(open, 0, fn char, acc ->
-        acc * 5 + @autocorrect[@block[char]]
-      end)
-
-    {:ok, score}
   end
 end
